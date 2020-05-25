ea248ae28ac8dd4e2d8b2d9bab1758aa1cbea430
jayh475 <57130830+jayh475@users.noreply.github.com>
2020-05-25 09:47:53 +0200

    project structuur helemaal omgegooid

--- IPASS_Project_Occasions/src/test/java/cuba/de/jayh/everything/model/AccountTest.java
+++ IPASS_Project_Occasions/src/test/java/cuba/de/jayh/everything/model/AccountTest.java
@@ -7,78 +7,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
-    private Account a1,a2,a3,a4;
+    private Account a1, a2, a3, a4;
 
 
     @BeforeEach
-    public void init(){
-        a1 = new Account("jayh.decuba@gmail.com","123", UserType.SUPERUSER);
-        a2 = new Account("kees.jan@gmail.com","234",UserType.NORMALUSER);
-        a3 = new Account("beer.dejong@gmail.com","345",UserType.NORMALUSER);
+    public void init() {
 
     }
 
 
-
-    @Test
-    void getAccountType() {
-        Assert.assertEquals("SUPERUSER",a1.getAccountType().toString());
-        Assert.assertEquals("NORMALUSER", a2.getAccountType().toString());
-
-    }
-
-    @Test
-    void getAllAccounts() {
-
-
-
-    }
-
-    @Test
-    void getAccountInfo() {
-//        Assert.assertEquals(,a1.getAccountInfo());
-        System.out.println(a1.getAccountInfo());
-
-    }
-
-    @Test
-    void tryLogin() {
-
-    }
-
-    @Test
-    void getLoggedInAccount() {
-    }
-
-    @Test
-    void logoff() {
-    }
-
-    @Test
-    void loggedInAs() {
-    }
-
-    @Test
-    void getAccountId() {
-    }
-
-    @Test
-    void loginStatus() {
-    }
-
-    @Test
-    void addMessage() {
-    }
-
-    @Test
-    void giveMyMessages() {
-    }
-
-    @Test
-    void messageToAllAdmins() {
-    }
-
-    @Test
-    void setType() {
-    }
 }
--- IPASS_Project_Occasions/src/test/java/cuba/de/jayh/everything/model/CarTest.java
+++ IPASS_Project_Occasions/src/test/java/cuba/de/jayh/everything/model/CarTest.java
@@ -13,84 +13,10 @@ class CarTest {
     private Car c1;
 
     @BeforeEach
-    void test(){
-        c1 = new Car("Volkswagen Polo 1.6 AUTOMAAT ( schade )" , "", 2000, 2016,1800,"diesel","73-MG-HJ","Volkswagen","Polo 1.6" );
+    void test() {
+        c1 = new Car("Volkswagen Polo 1.6 AUTOMAAT ( schade )", "", 2000, 2016, 1800, "diesel", "73-MG-HJ", "Volkswagen", "Polo 1.6");
 
     }
 
 
-
-    @Test
-    void getName() {
-        Assert.assertEquals("Volkswagen Polo 1.6 AUTOMAAT ( schade )",c1.getName());
-    }
-
-    @Test
-    void getImage() {
-    }
-
-    @Test
-    void getKilometre() {
-    }
-
-    @Test
-    void getYearOfManufacture() {
-
-    }
-
-    @Test
-    void getPrice() {
-    }
-
-    @Test
-    void getFuelType() {
-    }
-
-    @Test
-    void getCars() {
-    }
-
-    @Test
-    void getLicensePlate() {
-    }
-
-    @Test
-    void getBrand() {
-    }
-
-    @Test
-    void setName() {
-    }
-
-    @Test
-    void setImage() {
-    }
-
-    @Test
-    void setKilometre() {
-    }
-
-    @Test
-    void setYearOfManufacture() {
-    }
-
-    @Test
-    void setPrice() {
-    }
-
-    @Test
-    void setFuelType() {
-    }
-
-    @Test
-    void setLicensePlate() {
-    }
-
-    @Test
-    void setBrand() {
-    }
-
-    @Test
-    void testEquals() {
-    }
 }
